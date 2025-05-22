5d8e5901d1e089e4795802234df6170f74c7b589
joaodecaprio <joao.agmont@sou.inteli.edu.br>
2025-05-22 16:27:05 -0300

    Add files via upload

---
+++ mvc-boilerplate-main/server.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const db = require('./config/db');
+const cors = require('cors');
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+db.connect()
+  .then(() => {
+    console.log('Conectado ao banco de dados PostgreSQL');
+
+    app.use(express.json());
+
+    const userRoutes = require('./routes/index');
+    app.use('/users', userRoutes);
+
+    const frontendRoutes = require('./routes/index');
+    app.use('/', frontendRoutes);
+
+    // Middleware para lidar com erros de rota não encontrada
+    app.use((req, res, next) => {
+      res.status(404).send('Página não encontrada');
+    });
+
+    // Middleware para lidar com erros internos do servidor
+    app.use((err, req, res, next) => {
+      console.error(err.stack);
+      res.status(500).send('Erro no servidor');
+    });
+
+    const runSQLScript = require('./scripts/runSQLScript')
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, async() => {
+      await runSQLScript()
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('Erro ao conectar ao banco de dados:', err);
+  });
+
+  // server.js
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+const port = 3000;
+
+// Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+// Usando as rotas definidas
+app.use('/api', routes);
+
