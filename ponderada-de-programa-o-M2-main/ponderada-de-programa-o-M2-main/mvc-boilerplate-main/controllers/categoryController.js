2a0e92e3c9c1a11f319d3ccabad3875ecc1e3f92
joaodecaprio <joao.agmont@sou.inteli.edu.br>
2025-06-08 22:47:08 -0300

    Update categoryController.js

--- ponderada-de-programa-o-M2-main/ponderada-de-programa-o-M2-main/mvc-boilerplate-main/controllers/categoryController.js
+++ ponderada-de-programa-o-M2-main/ponderada-de-programa-o-M2-main/mvc-boilerplate-main/controllers/categoryController.js
@@ -9,19 +9,52 @@ exports.renderPage = async (req, res) => {
   }
 };
 
-exports.listarTarefas = async (req, res) => {
-  const tasks = await task.getAll();
-  const categories = await Category.getAll();
-  res.render('layout/task', { tasks, categories, pageTitle: 'Tarefas' });
+exports.getAll = async (req, res) => {
+  try {
+    const categories = await Category.getAll();
+    res.json(categories);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar categorias' });
+  }
+};
+
+exports.getById = async (req, res) => {
+  try {
+    const category = await Category.getById(req.params.id);
+    if (category) {
+      res.json(category);
+    } else {
+      res.status(404).json({ error: 'Categoria não encontrada' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar categoria' });
+  }
 };
 
 exports.create = async (req, res) => {
   try {
     const { name, description } = req.body;
     await Category.create({ name, description });
-    // Após criar, redireciona para a página de categorias
     res.redirect('/category');
   } catch (error) {
     res.status(500).send('Erro ao criar categoria');
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+    const updated = await Category.update(req.params.id, req.body);
+    res.json(updated);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao atualizar categoria' });
+  }
+};
+
+exports.delete = async (req, res) => {
+  try {
+    const deleted = await Category.delete(req.params.id);
+    res.json({ success: deleted });
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao deletar categoria' });
   }
 };
